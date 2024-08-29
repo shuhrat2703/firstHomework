188c23c84c9f1175ccda7d50186df88ed97efa3d
user <jakhongirislomjonov@gmail.com>
2024-08-29 11:52:19 +0300

    first Task

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Problem - 1
+        System.out.println("*********************");
+        System.out.println("Name:  Shuhrat ");
+        System.out.println("Surname: Egamberdiyev ");
+        System.out.println("Email: shuhrategamberdiyev@gmail.com");
+        System.out.println("Address: Tashkent, Uzbekistan");
+    }
+}
