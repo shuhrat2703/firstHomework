007ace71be7d0e9ea7852846418bb0e9ee66a2cf
Shuhrategamberdiyev <jakhongirislomjonov@gmail.com>
2024-08-29 21:06:12 +0500

    second Lesson

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -48,7 +49,40 @@ public class Main {
         System.out.println("The area is: " + area);
         System.out.println("The volume of the Triangular prism is: " + area * length);  */
 
-        // Problem - 6
+
+       /* long totalMilliSecond = System.currentTimeMillis();
+        long totalSecond = totalMilliSecond / 1000;
+        long currentSecond = totalSecond % 60;
+        long totalMinute = totalSecond / 60;
+        long currentMinute = totalMinute % 60;
+        long totalHour = totalMinute / 60;
+        long currentHour = totalHour % 24;
+        System.out.println("Current time is " + currentHour + ":" + currentMinute + ":" + currentSecond);*/
+
+
+        /*double number = Math.random();
+        System.out.println(number);
+        Random random = new Random();
+        int a = random.nextInt(1,30);
+        System.out.println(a);
+        System.out.println((int) (number*10));*/
+
+
+     /*   double a = 6.5;
+        a += 1;
+        System.out.println(a);
+        a = 6;
+        a /=2;
+        System.out.println(a);*/
+
+
+       /* int number1 = (int) (System.currentTimeMillis() % 10);
+        int number2 = (int) (System.currentTimeMillis() / 10 % 10);
+        System.out.println("What will be " + number1 + "+" + number2 + " ?");
+        Scanner scanner = new Scanner(System.in);
+        int answer = scanner.nextInt();
+        System.out.println(number1 + "+" + number2 + "=" + answer + " " + "is " + (number1 + number2 == answer));*/
+
 
 
     }
